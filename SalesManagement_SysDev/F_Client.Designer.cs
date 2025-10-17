993597b5bd86a88b792845d552c62a3e51cf3d84
OIC-2024-1B17KN-Takezawa <c4160@oic.jp>
2025-10-17 17:36:24 +0900

    顧客管理画面

--- SalesManagement_SysDev/F_Client.Designer.cs
+++ SalesManagement_SysDev/F_Client.Designer.cs
@@ -29,25 +29,318 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            dataGridView1 = new DataGridView();
+            textBox8 = new TextBox();
+            checkBox1 = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(361, 125);
+            label1.BackColor = Color.SteelBlue;
+            label1.Font = new Font("Noto Sans JP", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(1, -1);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(31, 15);
+            label1.Size = new Size(1563, 100);
             label1.TabIndex = 0;
-            label1.Text = "顧客";
+            label1.Text = "顧客管理";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Crimson;
+            button1.Font = new Font("Noto Sans JP", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(1446, 47);
+            button1.Name = "button1";
+            button1.Size = new Size(104, 42);
+            button1.TabIndex = 1;
+            button1.Text = "閉じる";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 135);
+            button2.Name = "button2";
+            button2.Size = new Size(141, 62);
+            button2.TabIndex = 2;
+            button2.Text = "顧客検索";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 215);
+            button3.Name = "button3";
+            button3.Size = new Size(141, 62);
+            button3.TabIndex = 3;
+            button3.Text = "更新";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 295);
+            button4.Name = "button4";
+            button4.Size = new Size(141, 62);
+            button4.TabIndex = 4;
+            button4.Text = "顧客登録";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(12, 375);
+            button5.Name = "button5";
+            button5.Size = new Size(141, 62);
+            button5.TabIndex = 5;
+            button5.Text = "クリア";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(12, 455);
+            button6.Name = "button6";
+            button6.Size = new Size(141, 62);
+            button6.TabIndex = 6;
+            button6.Text = "一覧表示";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(12, 535);
+            button7.Name = "button7";
+            button7.Size = new Size(141, 62);
+            button7.TabIndex = 7;
+            button7.Text = "一覧非表示";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Noto Sans JP", 10.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(230, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 32);
+            label2.TabIndex = 8;
+            label2.Text = "顧客ID";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Noto Sans JP", 10.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(207, 245);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 32);
+            label3.TabIndex = 9;
+            label3.Text = "郵便番号";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Noto Sans JP", 10.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(510, 165);
+            label4.Name = "label4";
+            label4.Size = new Size(80, 32);
+            label4.TabIndex = 10;
+            label4.Text = "顧客名";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Noto Sans JP", 10.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(865, 164);
+            label5.Name = "label5";
+            label5.Size = new Size(101, 32);
+            label5.TabIndex = 11;
+            label5.Text = "営業所ID";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Noto Sans JP", 10.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(532, 245);
+            label6.Name = "label6";
+            label6.Size = new Size(58, 32);
+            label6.TabIndex = 12;
+            label6.Text = "住所";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Noto Sans JP", 10.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(207, 325);
+            label8.Name = "label8";
+            label8.Size = new Size(102, 32);
+            label8.TabIndex = 14;
+            label8.Text = "電話番号";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Noto Sans JP", 10.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(615, 325);
+            label9.Name = "label9";
+            label9.Size = new Size(51, 32);
+            label9.TabIndex = 15;
+            label9.Text = "FAX";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Noto Sans JP", 10.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(1321, 324);
+            label10.Name = "label10";
+            label10.Size = new Size(124, 32);
+            label10.TabIndex = 16;
+            label10.Text = "現在の日付";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Noto Sans JP", 10.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(1197, 148);
+            label7.Name = "label7";
+            label7.Size = new Size(124, 32);
+            label7.TabIndex = 17;
+            label7.Text = "非表示理由";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(315, 167);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(155, 31);
+            textBox1.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(600, 167);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(245, 31);
+            textBox2.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(970, 168);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(155, 31);
+            textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(315, 247);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(169, 31);
+            textBox4.TabIndex = 21;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(600, 247);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(245, 31);
+            textBox5.TabIndex = 22;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(315, 327);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(275, 31);
+            textBox6.TabIndex = 23;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(672, 327);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(307, 31);
+            textBox7.TabIndex = 24;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(230, 408);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(1265, 415);
+            dataGridView1.TabIndex = 25;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(1207, 183);
+            textBox8.Multiline = true;
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(332, 114);
+            textBox8.TabIndex = 26;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(1394, 148);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(145, 29);
+            checkBox1.TabIndex = 27;
+            checkBox1.Text = "表示 / 非表示";
+            checkBox1.UseVisualStyleBackColor = true;
             // 
             // F_Client
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.LightBlue;
+            ClientSize = new Size(1562, 834);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox8);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "F_Client";
             Text = "F_Client";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +348,31 @@
         #endregion
 
         private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private DataGridView dataGridView1;
+        private TextBox textBox8;
+        private CheckBox checkBox1;
     }
 }
