c684e826421d5fadc8dd45eb4338acf6f5d0d47c
Your Name <c1135@oic.jp>
2025-10-17 17:04:38 +0900

    パスワード変更画面

--- SalesManagement_SysDev/F_ChangePassword.Designer.cs
+++ SalesManagement_SysDev/F_ChangePassword.Designer.cs
@@ -28,18 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            CP_lblPassword = new Label();
+            CP_lbID = new Label();
+            CP_lbtel = new Label();
+            CP_lbNewpass = new Label();
+            CP_lbpass = new Label();
+            CP_txFone = new TextBox();
+            CP_txLoginID = new TextBox();
+            CP_txNewpass = new TextBox();
+            CP_txCheckPass = new TextBox();
+            CP_bteye = new Button();
+            CP_bteye2 = new Button();
+            CP_btnTouroku = new Button();
+            CP_btnClose = new Button();
             SuspendLayout();
             // 
-            // label1
+            // CP_lblPassword
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(396, 226);
-            label1.Name = "label1";
-            label1.Size = new Size(51, 15);
-            label1.TabIndex = 0;
-            label1.Text = "パスワード";
-            
+            CP_lblPassword.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            CP_lblPassword.AutoSize = true;
+            CP_lblPassword.Font = new Font("Noto Sans JP", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            CP_lblPassword.Location = new Point(161, 44);
+            CP_lblPassword.Name = "CP_lblPassword";
+            CP_lblPassword.Size = new Size(183, 35);
+            CP_lblPassword.TabIndex = 0;
+            CP_lblPassword.Text = "パスワード変更\r\n";
+            // 
+            // CP_lbID
+            // 
+            CP_lbID.AutoSize = true;
+            CP_lbID.Font = new Font("Noto Sans JP", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            CP_lbID.Location = new Point(32, 106);
+            CP_lbID.Name = "CP_lbID";
+            CP_lbID.Size = new Size(186, 24);
+            CP_lbID.TabIndex = 1;
+            CP_lbID.Text = "ログインIDまたは社員ID";
+            // 
+            // CP_lbtel
+            // 
+            CP_lbtel.AutoSize = true;
+            CP_lbtel.Font = new Font("Noto Sans JP", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            CP_lbtel.Location = new Point(275, 106);
+            CP_lbtel.Name = "CP_lbtel";
+            CP_lbtel.Size = new Size(74, 24);
+            CP_lbtel.TabIndex = 2;
+            CP_lbtel.Text = "電話番号";
+            // 
+            // CP_lbNewpass
+            // 
+            CP_lbNewpass.AutoSize = true;
+            CP_lbNewpass.Font = new Font("Noto Sans JP", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            CP_lbNewpass.Location = new Point(32, 170);
+            CP_lbNewpass.Name = "CP_lbNewpass";
+            CP_lbNewpass.Size = new Size(138, 24);
+            CP_lbNewpass.TabIndex = 3;
+            CP_lbNewpass.Text = "新しいパスワード";
+            // 
+            // CP_lbpass
+            // 
+            CP_lbpass.AutoSize = true;
+            CP_lbpass.Font = new Font("Noto Sans JP", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            CP_lbpass.Location = new Point(275, 170);
+            CP_lbpass.Name = "CP_lbpass";
+            CP_lbpass.Size = new Size(58, 24);
+            CP_lbpass.TabIndex = 4;
+            CP_lbpass.Text = "確認用";
+            // 
+            // CP_txFone
+            // 
+            CP_txFone.Location = new Point(32, 128);
+            CP_txFone.Name = "CP_txFone";
+            CP_txFone.Size = new Size(129, 23);
+            CP_txFone.TabIndex = 5;
+            // 
+            // CP_txLoginID
+            // 
+            CP_txLoginID.Location = new Point(278, 128);
+            CP_txLoginID.Name = "CP_txLoginID";
+            CP_txLoginID.Size = new Size(129, 23);
+            CP_txLoginID.TabIndex = 6;
+            // 
+            // CP_txNewpass
+            // 
+            CP_txNewpass.Location = new Point(32, 193);
+            CP_txNewpass.Name = "CP_txNewpass";
+            CP_txNewpass.Size = new Size(178, 23);
+            CP_txNewpass.TabIndex = 7;
+            // 
+            // CP_txCheckPass
+            // 
+            CP_txCheckPass.Location = new Point(272, 193);
+            CP_txCheckPass.Name = "CP_txCheckPass";
+            CP_txCheckPass.Size = new Size(178, 23);
+            CP_txCheckPass.TabIndex = 8;
+            // 
+            // CP_bteye
+            // 
+            CP_bteye.BackColor = Color.SkyBlue;
+            CP_bteye.Location = new Point(216, 192);
+            CP_bteye.Name = "CP_bteye";
+            CP_bteye.Size = new Size(23, 24);
+            CP_bteye.TabIndex = 9;
+            CP_bteye.Text = "👁";
+            CP_bteye.UseVisualStyleBackColor = false;
+            // 
+            // CP_bteye2
+            // 
+            CP_bteye2.BackColor = Color.SkyBlue;
+            CP_bteye2.Location = new Point(456, 193);
+            CP_bteye2.Name = "CP_bteye2";
+            CP_bteye2.Size = new Size(23, 24);
+            CP_bteye2.TabIndex = 10;
+            CP_bteye2.Text = "👁";
+            CP_bteye2.UseVisualStyleBackColor = false;
+            // 
+            // CP_btnTouroku
+            // 
+            CP_btnTouroku.BackColor = Color.RoyalBlue;
+            CP_btnTouroku.Font = new Font("メイリオ", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            CP_btnTouroku.ForeColor = Color.White;
+            CP_btnTouroku.Location = new Point(297, 239);
+            CP_btnTouroku.Margin = new Padding(2);
+            CP_btnTouroku.Name = "CP_btnTouroku";
+            CP_btnTouroku.Size = new Size(80, 27);
+            CP_btnTouroku.TabIndex = 11;
+            CP_btnTouroku.Text = "登録";
+            CP_btnTouroku.UseVisualStyleBackColor = false;
+            // 
+            // CP_btnClose
+            // 
+            CP_btnClose.BackColor = Color.RoyalBlue;
+            CP_btnClose.Font = new Font("メイリオ", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            CP_btnClose.ForeColor = Color.White;
+            CP_btnClose.Location = new Point(411, 239);
+            CP_btnClose.Margin = new Padding(2);
+            CP_btnClose.Name = "CP_btnClose";
+            CP_btnClose.Size = new Size(80, 27);
+            CP_btnClose.TabIndex = 12;
+            CP_btnClose.Text = "閉じる";
+            CP_btnClose.UseVisualStyleBackColor = false;
             // 
             // F_ChangePassword
             // 
@@ -47,15 +174,39 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.SkyBlue;
             ClientSize = new Size(524, 289);
-            Controls.Add(label1);
+            Controls.Add(CP_btnClose);
+            Controls.Add(CP_btnTouroku);
+            Controls.Add(CP_bteye2);
+            Controls.Add(CP_bteye);
+            Controls.Add(CP_txCheckPass);
+            Controls.Add(CP_txNewpass);
+            Controls.Add(CP_txLoginID);
+            Controls.Add(CP_txFone);
+            Controls.Add(CP_lbpass);
+            Controls.Add(CP_lbNewpass);
+            Controls.Add(CP_lbtel);
+            Controls.Add(CP_lbID);
+            Controls.Add(CP_lblPassword);
             Name = "F_ChangePassword";
-            Text = "F_ChangePassword";
+            Text = "パスワード変更画面";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
+        private Label CP_lblPassword;
+        private Label CP_lbID;
+        private Label CP_lbtel;
+        private Label CP_lbNewpass;
+        private Label CP_lbpass;
+        private TextBox CP_txFone;
+        private TextBox CP_txLoginID;
+        private TextBox CP_txNewpass;
+        private TextBox CP_txCheckPass;
+        private Button CP_bteye;
+        private Button CP_bteye2;
+        private Button CP_btnTouroku;
+        private Button CP_btnClose;
     }
 }
