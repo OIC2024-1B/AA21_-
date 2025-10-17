80df191410ea6d12eb947d75a854b366bd372532
Your Name <c1135@oic.jp>
2025-10-17 15:33:26 +0900

    a

--- SalesManagement_SysDev/F_login.Designer.cs
+++ SalesManagement_SysDev/F_login.Designer.cs
@@ -28,52 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_CleateDabase = new System.Windows.Forms.Button();
-            this.Lbl_tag = new System.Windows.Forms.Label();
-            this.btn_InsertSampleData = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            btn_CleateDabase = new Button();
+            Lbl_tag = new Label();
+            btn_InsertSampleData = new Button();
+            button1 = new Button();
+            SuspendLayout();
             // 
             // btn_CleateDabase
             // 
-            this.btn_CleateDabase.Location = new System.Drawing.Point(661, 319);
-            this.btn_CleateDabase.Name = "btn_CleateDabase";
-            this.btn_CleateDabase.Size = new System.Drawing.Size(106, 49);
-            this.btn_CleateDabase.TabIndex = 0;
-            this.btn_CleateDabase.Text = "データベース生成";
-            this.btn_CleateDabase.UseVisualStyleBackColor = true;
-            this.btn_CleateDabase.Click += new System.EventHandler(this.btn_CleateDabase_Click);
+            btn_CleateDabase.Location = new Point(771, 399);
+            btn_CleateDabase.Margin = new Padding(4, 4, 4, 4);
+            btn_CleateDabase.Name = "btn_CleateDabase";
+            btn_CleateDabase.Size = new Size(124, 61);
+            btn_CleateDabase.TabIndex = 0;
+            btn_CleateDabase.Text = "データベース生成";
+            btn_CleateDabase.UseVisualStyleBackColor = true;
+            btn_CleateDabase.Click += btn_CleateDabase_Click;
             // 
             // Lbl_tag
             // 
-            this.Lbl_tag.AutoSize = true;
-            this.Lbl_tag.Location = new System.Drawing.Point(266, 161);
-            this.Lbl_tag.Name = "Lbl_tag";
-            this.Lbl_tag.Size = new System.Drawing.Size(224, 12);
-            this.Lbl_tag.TabIndex = 1;
-            this.Lbl_tag.Text = "このページはログイン画面として作成してください";
+            Lbl_tag.AutoSize = true;
+            Lbl_tag.Location = new Point(310, 201);
+            Lbl_tag.Margin = new Padding(4, 0, 4, 0);
+            Lbl_tag.Name = "Lbl_tag";
+            Lbl_tag.Size = new Size(223, 15);
+            Lbl_tag.TabIndex = 1;
+            Lbl_tag.Text = "このページはログイン画面として作成してください";
             // 
             // btn_InsertSampleData
             // 
-            this.btn_InsertSampleData.Location = new System.Drawing.Point(661, 389);
-            this.btn_InsertSampleData.Name = "btn_InsertSampleData";
-            this.btn_InsertSampleData.Size = new System.Drawing.Size(106, 49);
-            this.btn_InsertSampleData.TabIndex = 0;
-            this.btn_InsertSampleData.Text = "サンプルデータ登録";
-            this.btn_InsertSampleData.UseVisualStyleBackColor = true;
-            this.btn_InsertSampleData.Click += new System.EventHandler(this.btn_InsertSampleData_Click);
+            btn_InsertSampleData.Location = new Point(771, 486);
+            btn_InsertSampleData.Margin = new Padding(4, 4, 4, 4);
+            btn_InsertSampleData.Name = "btn_InsertSampleData";
+            btn_InsertSampleData.Size = new Size(124, 61);
+            btn_InsertSampleData.TabIndex = 0;
+            btn_InsertSampleData.Text = "サンプルデータ登録";
+            btn_InsertSampleData.UseVisualStyleBackColor = true;
+            btn_InsertSampleData.Click += btn_InsertSampleData_Click;
             // 
-            // F_Login
+            // button1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Lbl_tag);
-            this.Controls.Add(this.btn_InsertSampleData);
-            this.Controls.Add(this.btn_CleateDabase);
-            this.Name = "F_Login";
-            this.Text = "販売管理システムログイン画面";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            button1.Location = new Point(314, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // F_login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(933, 562);
+            Controls.Add(button1);
+            Controls.Add(Lbl_tag);
+            Controls.Add(btn_InsertSampleData);
+            Controls.Add(btn_CleateDabase);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "F_login";
+            Text = "販売管理システムログイン画面";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -81,5 +96,6 @@
         private System.Windows.Forms.Button btn_CleateDabase;
         private System.Windows.Forms.Label Lbl_tag;
         private System.Windows.Forms.Button btn_InsertSampleData;
+        private Button button1;
     }
 }
