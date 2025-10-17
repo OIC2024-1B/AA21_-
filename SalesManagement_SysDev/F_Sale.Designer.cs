3a0106772c927659d6da2e9a44ad262c390228aa
Your Name <c1135@oic.jp>
2025-10-17 15:54:48 +0900

    フォーム作成

---
+++ SalesManagement_SysDev/F_Sale.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace SalesManagement_SysDev
+{
+    partial class F_Sale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new Point(454, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new Size(31, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "売上";
+            // 
+            // F_Sale
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(this.label1);
+            Name = "F_Sale";
+            Text = "F_Sale";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+    }
+}
